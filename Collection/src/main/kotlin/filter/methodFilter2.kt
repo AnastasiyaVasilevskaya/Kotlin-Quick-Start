68b3ed7371326a2d5851079aae98a30a43bc3b28
Anastasiya Vasilevskaya <94404108+nastyavasilevskaya@users.noreply.github.com>
2023-11-29 14:00:23 +0300

    Delete Collection/src/main/kotlin/filter directory

--- Collection/src/main/kotlin/filter/methodFilter2.kt
+++
@@ -1,9 +0,0 @@
-package filter
-
-fun main() {
-    val names = listOf<String>("Андрей", "Маша", "Олег", "Артем")
-    val listOfANames = names.filter{ it.startsWith("А")}
-    for(i in listOfANames){
-        println(i)
-    }
-}
